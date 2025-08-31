981b2eaf95169ae3456f5a960f51c84f94d1603a
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2025-08-31 16:31:03 +0700

    feat(payment-service): add translation with i18n for payment-service

--- services/payment-service/src/main/java/dev/edu/ngochandev/paymentservice/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/dev/edu/ngochandev/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package dev.edu.ngochandev.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dev.edu.ngochandev.paymentservice", "dev.edu.ngochandev.common"})
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
---
+++ services/payment-service/src/main/java/dev/edu/ngochandev/paymentservice/configs/LocalResolver.java
@@ -0,0 +1,19 @@
+package dev.edu.ngochandev.paymentservice.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class LocalResolver extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages_payment", "messages_common");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3600);
+        return messageSource;
+    }
+}
